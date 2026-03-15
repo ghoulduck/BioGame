9d6b4a54df7e9393b5a55af6649790a43ef8e5e1
JV <jvcereal@gmail.com>
2026-03-15 15:08:16 -0400

    Working on adding windows Starting on game functions Last will be to implement GUI and interactivity Re-writing Producer.java Have to implement in all child hierarchies

--- src/Driver.java
+++ src/Driver.java
@@ -4,5 +4,6 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println("Welcome to BioFactory!");
         Game game = new Game();
+        // need to implement game loop
     }
 }
